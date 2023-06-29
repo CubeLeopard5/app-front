197f7d937c9951b6e7ad40541f5238103baaa236
Adrien Michaud <adrien.michaud@epitech.eu>
2023-06-29 15:15:37 +0200

    test registration and login

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,44 @@
 <template>
-    <a-input v-model:value="value" placeholder="Basic usage" v-on:keyup.enter="handleOk"/>
+<div>
+    <a-input v-model:value="username" placeholder="Basic usage"/>
+    <a-input v-model:value="email" placeholder="Basic usage" />
+    <a-input v-model:value="password" placeholder="Basic usage" />
+    <a-button type="primary" @click="handleOk">
+        send
+    </a-button>
+    gfdd + ljjhg
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 export default defineComponent({
     setup() {
-        const value = ref<string>('');
+        const username = ref<string>('');
+        const email = ref<string>('');
+        const password = ref<string>('');
         const handleOk = () => {
             let headers = {
 				//"Authorization": (obj.accessToken) ? `Bearer ${this.accessToken}` : '',
 			};
 			headers['Accept'] = 'application/json';
 			headers['Content-Type'] = 'application/json';
-			const url = 'http://localhost:8000/';
-			return fetch(`${url}`, {
-				method: 'GET',
+			const url = 'http://localhost:8000';
+            const endpoint = 'login';
+            let obj = {};
+            obj['username'] = username.value;
+            obj['email'] = email.value;
+            obj['password'] = password.value;
+			return fetch(`${url}/${endpoint}`, {
+				method: 'POST',
 				headers: headers,
+                body: JSON.stringify(obj),
 			}).then(response => response.json());
         };
         return {
-            value,
+            username,
+            email,
+            password,
             handleOk,
         };
     },
