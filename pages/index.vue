e8e9db64d000cc1dd8f0b255378e508f6849d774
Adrien Michaud <adrien.michaud@epitech.eu>
2023-06-29 11:27:54 +0200

    add antd

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+    <a-input v-model:value="value" placeholder="Basic usage" v-on:keyup.enter="handleOk"/>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+    setup() {
+        const value = ref<string>('');
+        const handleOk = () => {
+            let headers = {
+				//"Authorization": (obj.accessToken) ? `Bearer ${this.accessToken}` : '',
+			};
+			headers['Accept'] = 'application/json';
+			headers['Content-Type'] = 'application/json';
+			const url = 'http://localhost:8000/';
+			return fetch(`${url}`, {
+				method: 'GET',
+				headers: headers,
+			}).then(response => response.json());
+        };
+        return {
+            value,
+            handleOk,
+        };
+    },
+});
+</script>
